39cd795fe2ff834f60b0fbaec83baa1d6105e644
Tobias Hochgürtel <tobias.hochguertel@valtech.de>
2018-08-13 20:07:23 +0200

    green

--- src/test/java/de/swk/brunswick/codecracker/CodeCrackerTest.java
+++ src/test/java/de/swk/brunswick/codecracker/CodeCrackerTest.java
@@ -28,10 +28,15 @@ public class CodeCrackerTest {
     public void isLetterADecodedToExclaimationMark() {
         Assert.assertEquals("!", decode("a"));
     }
+    @Test
+    public void isLetterBDecodedToExclaimationMark() {
+        Assert.assertEquals(")", decode("b"));
+    }
 
     private String decode(String a) {
         Map<Character, Character> codeMap =  new HashMap<>();
         codeMap.put('a', '!');
+        codeMap.put('b', ')');
         return codeMap.get(a.charAt(0)).toString();
     }
 }
