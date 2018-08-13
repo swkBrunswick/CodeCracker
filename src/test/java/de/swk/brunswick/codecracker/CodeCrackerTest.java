c7f510eb800f75309139d0ccbb6bebcaaf01737f
Tobias Hochgürtel <tobias.hochguertel@valtech.de>
2018-08-13 21:10:10 +0200

    green

--- src/test/java/de/swk/brunswick/codecracker/CodeCrackerTest.java
+++ src/test/java/de/swk/brunswick/codecracker/CodeCrackerTest.java
@@ -1,6 +1,5 @@
 package de.swk.brunswick.codecracker;
 
-import java.util.Map;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -19,10 +18,6 @@ public class CodeCrackerTest {
     @Test
     public void emptyStringDeliversemptyRsult() {
         Assert.assertEquals("", codeCracker.encode());
-    }
-
-    private String encode() {
-        return codeCracker.encode();
     }
 
     @Test
@@ -46,19 +41,4 @@ public class CodeCrackerTest {
         Assert.assertEquals(expected, codeCracker.encode("abc"));
     }
 
-    private String encode(String word) {
-        return codeCracker.encode(word);
-    }
-
-    private void testALetter(String s, char c) {
-        codeCracker.testALetter(s, c);
-    }
-
-    private Character encode(Character toDecodeCharacter) {
-        return codeCracker.encode(toDecodeCharacter);
-    }
-
-    private Map<Character, Character> getCodingMap() {
-        return codeCracker.getCodingMap();
-    }
 }
