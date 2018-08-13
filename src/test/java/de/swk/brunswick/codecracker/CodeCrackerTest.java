dbf6f26db9395e55c351c672c10e52f34b3ab82d
Tobias Hochgürtel <tobias.hochguertel@valtech.de>
2018-08-13 19:07:10 +0200

    Adds initial project files.^

---
+++ src/test/java/de/swk/brunswick/codecracker/CodeCrackerTest.java
@@ -0,0 +1,17 @@
+package de.swk.brunswick.codecracker;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * CodeCrackerTest
+ */
+public class CodeCrackerTest {
+
+    @Test
+    public void doesThisWork() {
+        Assert.assertNotNull(new Object());
+    }
+
+
+}
